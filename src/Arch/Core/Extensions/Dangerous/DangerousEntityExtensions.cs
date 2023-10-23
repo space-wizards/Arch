36f9c751d3411455a64f6996cd80ff32a57c63a1
metalgearsloth <comedian_vs_clown@hotmail.com>
2023-10-23 21:28:50 +1100

    Add EntityReference dangerous extension

--- src/Arch/Core/Extensions/Dangerous/DangerousEntityExtensions.cs
+++ src/Arch/Core/Extensions/Dangerous/DangerousEntityExtensions.cs
@@ -8,10 +8,10 @@ public static class DangerousEntityExtensions
 {
     /// <summary>
     ///     Creates an <see cref="Entity"/> struct and returns it.
-    ///     Does not create an <see cref="Entity"/> in the world, just the plain struct. 
+    ///     Does not create an <see cref="Entity"/> in the world, just the plain struct.
     /// </summary>
     /// <param name="id">Its id.</param>
-    /// <param name="world">Its world id.</param>
+    /// <param name="world">Its world id. Unused if PURE_ECS is set.</param>
     /// <returns>The new <see cref="Entity"/>.</returns>
     public static Entity CreateEntityStruct(int id, int world)
     {
@@ -19,6 +19,25 @@ public static class DangerousEntityExtensions
         return new Entity(id, 0);
 #else
         return new Entity(id, world);
+#endif
+    }
+
+    /// <summary>
+    ///     Creates an <see cref="EntityReference"/> struct and returns it.
+    ///     Does not create an EntityReference, just the struct.
+    /// </summary>
+    /// <param name="id">Its id.</param>
+    /// <param name="version">The version of the entity.</param>
+    /// <param name="world">The world id. Unused if PURE_ECS is set.</param>
+    /// <returns>The <see cref="EntityReference"/></returns>
+    public static EntityReference CreateEntityReferenceStruct(int id, int version, int world)
+    {
+#if PURE_ECS
+        var ent = new Entity(id, 0);
+        return new EntityReference(ent, version);
+#else
+        var ent = new Entity(id, world);
+        return new EntityReference(ent, version);
 #endif
     }
 }
