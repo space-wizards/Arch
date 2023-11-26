9a3c13c3a3682b1bbad82ec51aae9a54c90087cf
metalgearsloth <comedian_vs_clown@hotmail.com>
2023-11-26 13:14:06 +1100

    a

--- src/Arch/Core/Jobs/Jobs.cs
+++ src/Arch/Core/Jobs/Jobs.cs
@@ -181,11 +181,9 @@ public class ChunkIterationJob<T> : IJob where T : IChunkJob
     /// </summary>
     public void Execute()
     {
-        ref var chunk = ref Chunks.DangerousGetReferenceAt(Start);
-
         for (var chunkIndex = 0; chunkIndex < Size; chunkIndex++)
         {
-            ref var currentChunk = ref Unsafe.Add(ref chunk, chunkIndex);
+            ref var currentChunk = ref Chunks[chunkIndex];
             Instance?.Execute(Start + chunkIndex, ref currentChunk);
         }
     }
